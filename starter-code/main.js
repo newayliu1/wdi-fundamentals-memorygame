94f5fe7450fb99353e36a5201c00c03fef866f2b
wei <wei@weis-MacBook-Air.local>
2017-01-04 14:40:59 -0500

    created conditionals

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,2 +1,8 @@
 console.log("JS file is connected to HTML! Woo!")
 var cardOne='queen', cardTwo='queen', cardThree='king', cardFour='king';
+if (cardTwo==cardFour){
+	alert("You found a match!");
+}
+else{
+	alert("Sorry, try again.");
+}
