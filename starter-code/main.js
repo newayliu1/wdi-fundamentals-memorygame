67bdedd1b88f458bb21d0361c0b3d64131d45c1d
“newayliu1” <newayliu1@gmail.com>
2017-01-07 21:10:29 -0500

    Created some dynamic game logic and completed the game.

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne='queen', cardTwo='queen', cardThree='king', cardFour='king';
-if (cardTwo==cardFour){
-	alert("You found a match!");
-}
-else{
-	alert("Sorry, try again.");
-}
+var cards=["queen","queen","king","king"];
+var cardsInPlay=[];
+
+// if (cards.cardTwo==cards.cardFour){
+// 	alert("You found a match!");
+// }
+// else{
+// 	alert("Sorry, try again.");
+// }
 
 var createCards=function(){
 var gameBoard = document.getElementById("game-board");
@@ -16,4 +18,47 @@ for(var i=0; i<4;i++)
 	gameBoard.appendChild(newDiv);
 }
 }
-createCards();
+var cardElements=document.getElementsByClassName("card");
+
+var createBoard=function(){
+	createCards();
+	for (var i=0;i<cards.length; i++){
+		cardElements[i].setAttribute('data-card',cards[i]);
+		cardElements[i].addEventListener('click',isTwocards);
+	}
+}
+var isTwocards = function(){
+	var dataCard = this.getAttribute('data-card');
+	cardsInPlay.push(dataCard);
+	if (dataCard =='king')
+	{
+		this.innerHTML ='<img src="my_king.png"/>';
+	}
+	else
+	{
+		this.innerHTML ='<img src="my_queen.png"/>';
+	}
+	// pause the excution for 1 sec.
+	setTimeout(function()
+	{
+		if (cardsInPlay.length===2)
+		{
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
+	}, 1000);
+}
+var isMatch = function (cards){
+	for (var i=0;i<cardElements.length; i++)
+	{
+		cardElements[i].innerHTML='';
+	}
+	if (cards[0]===cards[1])
+	{
+		alert("You found a match!");
+	}
+	else{
+		alert("Sorry, try again.");
+	}
+}
+createBoard();
